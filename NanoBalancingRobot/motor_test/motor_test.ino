0c595c6560287a433968772013953492ce8d44ef
Mateusz Gutowski <ma2tig@gmail.com>
2019-05-31 21:32:18 +0200

    test for two motors

--- NanoBalancingRobot/motor_test/motor_test.ino
+++ NanoBalancingRobot/motor_test/motor_test.ino
@@ -3,10 +3,16 @@
 #define MOTOR1_Right_PWM 5
 #define MOTOR1_Right_Enable 10
 
+#define MOTOR2_Left_PWM 6
+#define MOTOR2_Left_Enable 13
+#define MOTOR2_Right_PWM 9
+#define MOTOR2_Right_Enable 12
+
 enum rotation
 {
   LEFT,
-  RIGHT
+  RIGHT,
+  NONE
 };
 
 void set_motors_pins_output(void);
@@ -45,19 +51,25 @@ void loop() {
         rot = LEFT; 
       counter_dir = 1;
     }
-
+    
     counter += counter_dir;
     Serial.println(counter);
     if ( rot == LEFT) Serial.println("left");
     else Serial.println("right");
     motor_speed = map(counter,0,10000,100,200);
+    //motorControl(1,motor_speed,rot);
     motorControl(1,motor_speed,rot);
-    
+    motorControl(2,motor_speed,rot);
  }
 }
 
 
-
+void turnOffMotors()
+{
+  motorControl(1,0,NONE);
+  motorControl(2,0,NONE);
+  
+}
 void motorControl(uint8_t motor_number, uint8_t motor_speed, rotation motor_direction)
 {
   if (motor_number == 1)
@@ -79,7 +91,32 @@ void motorControl(uint8_t motor_number, uint8_t motor_speed, rotation motor_dire
       digitalWrite(MOTOR1_Right_Enable, HIGH);
       analogWrite(MOTOR1_Right_PWM, motor_speed);
     }
+    else
+      set_motors_pins_off;
+      
   }
+  else if (motor_number == 2)
+  {
+    if (motor_direction == LEFT)
+    {
+      digitalWrite(MOTOR2_Right_Enable, LOW);
+      analogWrite(MOTOR2_Right_PWM, 0);
+      
+      digitalWrite(MOTOR2_Left_Enable, HIGH);
+      analogWrite(MOTOR2_Left_PWM, motor_speed);
+      
+    }
+    else if(motor_direction == RIGHT)
+    {
+      digitalWrite(MOTOR2_Left_Enable, LOW);
+      analogWrite(MOTOR2_Left_PWM, 0);
+      
+      digitalWrite(MOTOR2_Right_Enable, HIGH);
+      analogWrite(MOTOR2_Right_PWM, motor_speed);
+    }
+  }
+    else
+      set_motors_pins_off;
 }
 void set_motors_pins_output(void)
 {
@@ -87,6 +124,11 @@ void set_motors_pins_output(void)
   pinMode(MOTOR1_Left_Enable, OUTPUT);
   pinMode(MOTOR1_Right_PWM, OUTPUT);
   pinMode(MOTOR1_Right_Enable, OUTPUT);
+
+  pinMode(MOTOR2_Left_PWM, OUTPUT);
+  pinMode(MOTOR2_Left_Enable, OUTPUT);
+  pinMode(MOTOR2_Right_PWM, OUTPUT);
+  pinMode(MOTOR2_Right_Enable, OUTPUT);
 }
 void set_motors_pins_off(void)
 {
@@ -94,4 +136,9 @@ void set_motors_pins_off(void)
   digitalWrite(MOTOR1_Right_Enable, LOW);
   analogWrite(MOTOR1_Left_PWM, 0);
   analogWrite(MOTOR1_Right_PWM, 0);
+
+  digitalWrite(MOTOR2_Left_Enable, LOW);
+  digitalWrite(MOTOR2_Right_Enable, LOW);
+  analogWrite(MOTOR2_Left_PWM, 0);
+  analogWrite(MOTOR2_Right_PWM, 0);
 }
